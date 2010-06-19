593534463dd996ea2f425842a0c931845e27f345
Ron Savage <ron@savage.net.au>
2010-06-19 15:24:01 +1000

    Add Pg-specific and SQLite-specific code

--- examples/table-info.pl
+++ examples/table-info.pl
@@ -9,7 +9,18 @@ use DBIx::Admin::TableInfo 2.0;
 
 # ---------------------
 
-my($dbh)	= DBI -> connect($ENV{'DBI_DSN'}, $ENV{'DBI_USER'}, $ENV{'DBI_PASS'});
-my($schema)	= $ENV{'DBI_DSN'} =~ /^dbi:Oracle/i ? uc $ENV{'DBI_USER'} : undef;
+my($attr) = {};
+my($dbh)  = DBI -> connect($ENV{'DBI_DSN'}, $ENV{'DBI_USER'}, $ENV{'DBI_PASS'}, $attr);
+
+if ($ENV{'DBI_DSN'} =~ /SQLite/i)
+{
+	$dbh -> do('PRAGMA foreign_keys = ON');
+}
+
+my($schema) = $ENV{'DBI_DSN'} =~ /^dbi:Oracle/i
+	? uc $ENV{'DBI_USER'}
+	: $ENV{'DBI_DSN'} =~ /^dbi:Pg/i
+	? 'public'
+	: undef;
 
 print Data::Dumper -> Dump([DBIx::Admin::TableInfo -> new(dbh => $dbh, schema => $schema) -> info()]);
