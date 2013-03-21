f2d68be87960601209e441ab1e4c6b23b81b53ac
ronsavage <ron@savage.net.au>
2013-03-21 13:24:25 +1100

    Allow user to set schema

--- examples/table.info.pl
+++ examples/table.info.pl
@@ -13,12 +13,15 @@ my($attr)              = {};
 $$attr{sqlite_unicode} = 1 if ($ENV{DBI_DSN} =~ /SQLite/i);
 my($dbh)               = DBI -> connect($ENV{DBI_DSN}, $ENV{DBI_USER}, $ENV{DBI_PASS}, $attr);
 
-$dbh -> do('PRAGMA foreign_keys = ON') if ($ENV{DBI_DSN} =~ /SQLite/i);
+$dbh -> do('PRAGMA foreign_keys = ON')           if ($ENV{DBI_DSN} =~ /SQLite/i);
+$dbh -> do("set search_path = $ENV{DBI_SCHEMA}") if ($ENV{DBI_SCHEMA});
 
 my($schema) = $ENV{DBI_DSN} =~ /^dbi:Oracle/i
 	? uc $ENV{DBI_USER}
 	: $ENV{DBI_DSN} =~ /^dbi:Pg/i
-	? ($ENV{DBI_SCHEMA} || 'public')
+	? $ENV{DBI_SCHEMA}
+	? undef
+	: 'public'
 	: undef;
 
 print Data::Dumper -> Dump([DBIx::Admin::TableInfo -> new(dbh => $dbh, schema => $schema) -> info()]);
