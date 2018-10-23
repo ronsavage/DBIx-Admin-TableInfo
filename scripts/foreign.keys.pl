821914e1785ab56481b07bad9506541d1093e5e4
ronsavage <ron@savage.net.au>
2018-10-23 17:37:31 +1100

    Update README. Fix typo in scripts/foreign.keys.pl

--- scripts/foreign.keys.pl
+++ scripts/foreign.keys.pl
@@ -17,7 +17,7 @@ my($info)              = DBIx::Admin::TableInfo -> new(dbh => $dbh) -> info;
 
 $dbh -> do('pragma foreign_keys = on') if ($ENV{DBI_DSN} =~ /SQLite/i);
 
-for my $table (qw/pricing_plans receipts)
+for my $table (qw/pricing_plans receipts/)
 {
 	print "Foreign keys for $table: \n", Dumper($$info{$table}{foreign_keys}), "\n";
 }
